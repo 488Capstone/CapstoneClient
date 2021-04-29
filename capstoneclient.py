96ba8c157e35c011a9a2fe834ac24959e12c8711
cjturne6 <55654880+cjturne6@users.noreply.github.com>
2021-04-28 21:26:28 -0500

    testing...

--- capstoneclient.py
+++ capstoneclient.py
@@ -142,7 +142,8 @@ def sense_soil():
 def water_scheduler(zone, duration, day, hour_time, minute_time):
     schedule = CronTab(user=True)
     command_string = './zone_control.py ' + zone + ' ' + str(duration)
-    job = schedule.new(command=command_string, comment=‘SillySprinkler’)
+    job = schedule.new(command=command_string)
+    #  comment=‘SillySprinkler’
     job.dow.on(day) # day must be three letter string, all caps
     job.minute.on(minute_time)
     job.hour.on(hour_time)
