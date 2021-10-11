ded440b54e391515112bf28a1b96e97e9dd535f2
dwollsch <dwollsch1@gmail.com>
2021-10-11 12:12:15 -0700

    zone_control updates - dynamic 9V supply

--- capstoneclient/gpio_control.py
+++ capstoneclient/gpio_control.py
@@ -59,12 +59,12 @@ def get_pin(pin):
 # name corresponds to the pin name from our pcb schematic, value is True or False
 def write_pin(pin, value):
     print(f"write_pin GPIO_SETUP_DONE {GPIO_SETUP_DONE}")
-    if not GPIO_SETUP_DONE:
-        setup_gpio()
+#    if not GPIO_SETUP_DONE:
+#        setup_gpio()
     pinnum = get_pin(pin)
     pinval = state_gpio(value)
     if pinnum is not None and pinval is not None:
-        GPIO.setup(pinnum, GPIO.OUT)
+        #GPIO.setup(pinnum, GPIO.OUT)
         return GPIO.output(pinnum, pinval)
 
 def cleanup():
