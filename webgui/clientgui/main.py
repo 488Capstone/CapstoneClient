c8090e16e4308ddd2a118a3eae11298f2bd374e3
dwollsch <dwollsch1@gmail.com>
2021-11-01 14:34:33 -0700

    added log file view

--- webgui/clientgui/main.py
+++ webgui/clientgui/main.py
@@ -10,6 +10,7 @@ from clientgui.db import get_db
 from clientgui.auth import login_required
 from sqlalchemy import text, exc
 import capstoneclient.zone_control_defs as zc
+#import subprocess as sp
 
 bp = Blueprint('main', __name__)
 
@@ -67,6 +68,51 @@ def my_cronschedule():
   #  for x in range(len(days)):
   #      return_str = return_str + "\nOn {}, zone 1 will run for {} minutes starting at {}:{}.".format(days[x][0], days[x][3], days[x][2], days[x][1]))
     return str(return_str)
+
+
+@bp.route('/clientlog', methods=('GET', 'POST'))
+@login_required
+def clientlog ():
+    clientDir = os.getenv('SIOclientDir')
+#    if request.method == 'POST':
+#        if request.form.get('Toggle Zone1') == 'Toggle Zone1':
+#            # pass
+#            #print("Toggle Zone1 Valve!!!")
+#            zc.toggle_valve(1)
+#        elif  request.form.get('Open Zone1') == 'Open Zone1':
+#            if clientDir is None:
+#                zc.open_valve(1)
+#            else:
+#                os.system(f"cd {clientDir}; ./runPy.sh ./zone_control.py z1 0 on")
+#        elif  request.form.get('Close Zone1') == 'Close Zone1':
+#            if clientDir is None:
+#                zc.close_valve(1)
+#            else:
+#                os.system(f"cd {clientDir}; ./runPy.sh ./zone_control.py z1 0 off")
+#        elif  request.form.get('Enable WaterDemo') == 'Enable WaterDemo':
+#            if clientDir is not None:
+#                os.system(f"cd {clientDir}; ./runPy.sh ./capstoneclient/demo.py")
+#        elif  request.form.get('Disable WaterDemo') == 'Disable WaterDemo':
+#            import capstoneclient.demo as demo
+#            demo.disable_demo_water_mode()
+##            if clientDir is not None:
+##                os.system(f"cd {clientDir}; ./runPy.sh ./capstoneclient/demo.py")
+#        else:
+#            # pass # unknown
+#            pass
+    #cronsched = my_cronschedule()
+    log_filename = clientDir + "/client_dev.log"
+#    log_fh = open(log_filename, 'r')
+#    log_fh.readlines
+#    log_fh.close()
+    cmd_stream = os.popen(f"tail --lines=80 {log_filename}")
+    logText = cmd_stream.read()
+    #DW 2021-11-01-14:24 flip the log file lines around so the latest log events are at the top
+    logList = logText.split('\n')
+    logList.reverse()
+    #print(logList)
+    logText = '\n'.join(logList)
+    return render_template('logview.html', logText=logText)
 
 
 
